5f0aad3299247220c328e8dbf5b3a3a3322e0a2b
PringleElUno <pringlel@oregonstate.edu>
2026-01-11 22:58:19 -0800

    Reworked the recursion lets see how this works

--- evaluateExpression.py
+++ evaluateExpression.py
@@ -1,8 +1,20 @@
 import unittest
 
 def evaluteString(input):
+    """
+    Recursviely evaluating a expression containing only positive integers
+    and '+' to use the correct operand.
+    We want to achieve 2+3+4 to equal 9.
+    """
     # A Base case is presented where we use an if statement to show '+' in an input
     if '+' not in input:
         return int(input) # we return that input that the user created as an integer of course
     
     # We developr our recursive case here
+    index = len(input) - 1  # whatever the user input is it will be subtracted by 1
+    while input[input] != '+': # We create a loop 
+        index -= 1 
+
+    left_part_of_expression = input[:idex]
+    right_number_of_expression = int(input[:index + 1:])
+
